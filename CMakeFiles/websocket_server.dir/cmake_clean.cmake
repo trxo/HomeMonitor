ab45143370787398c58744a1ce8606d6080150bb
trxo <trxo778@gmail.com>
2018-07-10 16:19:07 +0800

    improve

---
+++ CMakeFiles/websocket_server.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/websocket_server.dir/main.cpp.o"
+  "websocket_server.pdb"
+  "websocket_server"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/websocket_server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
