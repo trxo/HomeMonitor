c722a9c2cc524af8fd8a3e446404cf4ace32c72d
trxo <trxo778@gmail.com>
2018-10-22 23:57:46 +0800

    fix

--- main.cpp
+++ main.cpp
@@ -63,20 +63,22 @@ int main(int argc, char *argv[]) {
 
     ssize_t read_size;
     //Recevied message from client
-    while ((read_size = recv(client_sock, buffer, 2000, 0)) > 0) {
+    while ((read_size = recv(client_sock, buffer, BUF_SIZE, 0)) > 0) {
 
         printf("client_message size is: %d\n",read_size);
         if (!handshake) {
             handshake = doHandshake(buffer,client_sock);
             printf("handshake success...\n");
+            sendMsg("helllo",client_sock);
         } else {
             std::string res_str = frameDecode(buffer);
-            printf("recv data from client:%s\n",res_str.c_str());
+
+
+//            printf("recv data from client:%s\n",res_str);
             //将该消息回送客户端
             sendMsg(res_str,client_sock);
 
         }
-        memset(buffer,0, sizeof(buffer));
         bzero(buffer,BUF_SIZE);
     }
 
@@ -91,7 +93,7 @@ int main(int argc, char *argv[]) {
 
 int sendMsg(std::string string,int client_sock){
     std::string res;
-    frameEncode(string, res);
+    wsEncodeFrame(string, res,WS_TEXT_FRAME);
     return write(client_sock, res.c_str(), res.size());
 }
 
@@ -139,12 +141,7 @@ bool doHandshake(char *client_message,int client_sock)
     response += "Upgrade: websocket\r\n";
     response += "Connection: upgrade\r\n";
     response += "Sec-WebSocket-Accept: " + accept + "\r\n\r\n";
-    char p[2000];
-    response.copy(p, response.size(), 0);
-    *(p + response.size()) = '/0'; //要手动加上结束符
-    write(client_sock, p, strlen(p));
-//    write(client_sock, response.c_str(), response.size());
-
+    write(client_sock, response.c_str(), response.size());
     return true;
 }
 
@@ -231,24 +228,47 @@ std::string frameDecode(char *client_message) {
     return payloadData;
 }
 
-void frameEncode(std::string msg, std::string &outFrame) {
-    uint32_t messageLength = msg.size();
-    printf("messageLength: %d\n",messageLength);
+
+int wsEncodeFrame(std::string inMessage, std::string &outFrame, enum WS_FrameType frameType)
+{
+    int ret = WS_EMPTY_FRAME;
+    const uint32_t messageLength = inMessage.size();
+    if (messageLength > 32767)
+    {
+        // 暂不支持这么长的数据
+        return WS_ERROR_FRAME;
+    }
+
     uint8_t payloadFieldExtraBytes = (messageLength <= 0x7d) ? 0 : 2;
+    // header: 2字节, mask位设置为0(不加密), 则后面的masking key无须填写, 省略4字节
     uint8_t frameHeaderSize = 2 + payloadFieldExtraBytes;
     uint8_t *frameHeader = new uint8_t[frameHeaderSize];
     memset(frameHeader, 0, frameHeaderSize);
-    frameHeader[0] = static_cast<uint8_t>(0x80 | WS_TEXT_FRAME);
-    frameHeader[1] = static_cast<uint8_t>(messageLength);
-    std::cout<< frameHeader[1] << std::endl;
+    // fin位为1, 扩展位为0, 操作位为frameType
+    frameHeader[0] = static_cast<uint8_t>(0x80 | frameType);
+
+    // 填充数据长度
+    if (messageLength <= 0x7d)
+    {
+        frameHeader[1] = static_cast<uint8_t>(messageLength);
+    }
+    else
+    {
+        frameHeader[1] = 0x7e;
+        uint16_t len = htons(messageLength);
+        memcpy(&frameHeader[2], &len, payloadFieldExtraBytes);
+    }
+
+    // 填充数据
     uint32_t frameSize = frameHeaderSize + messageLength;
-    //printf("frameSize: %d\n",frameSize);
     char *frame = new char[frameSize + 1];
     memcpy(frame, frameHeader, frameHeaderSize);
-    memcpy(frame + frameHeaderSize, msg.c_str(), messageLength);
+    memcpy(frame + frameHeaderSize, inMessage.c_str(), messageLength);
     frame[frameSize] = '\0';
     outFrame = frame;
+
     delete[] frame;
     delete[] frameHeader;
+    return ret;
 }
 
